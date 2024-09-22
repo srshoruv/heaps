b9662b37e52deca8d91f57c329b14938905beed0
srshoruv <srshoruv2002@gmail.com>
2024-09-23 04:04:35 +0600

    priority queue for pairs

--- pqForObjects.cpp
+++ pqForObjects.cpp
@@ -18,15 +18,27 @@ public:
     }  
 };
 
+struct Compare {
+    bool operator() (pair<string, int> &p1, pair<string, int> &p2) {
+        return p1.second < p2.second;
+    }
+};
+
 int main(){
     priority_queue<Student> pq;
+
+    priority_queue<pair<string, int>, vector<pair<string, int>>,Compare> pqPair;
 
     pq.push(Student("Shoruv", 700));
     pq.push(Student("Shahidur", 900));
     pq.push(Student("Rahman", 800));
 
-    while(!pq.empty()) {
-        cout << pq.top().name << ", " << pq.top().marks << endl;
-        pq.pop();
+    pqPair.push(make_pair("Shoruv", 700));
+    pqPair.push(make_pair("Shahidur", 900));
+    pqPair.push(make_pair("Rahman", 800));
+
+    while(!pqPair.empty()) {
+        cout << pqPair.top().first << ", " << pqPair.top().second << endl;
+        pqPair.pop();
     }
 }
